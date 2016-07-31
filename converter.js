597c964b6bf6970cc8e074889559f9da7b0fbc9c
jkillz2020 <kilgore.josh@gmail.com>
2016-07-31 17:38:03 -0500

    add all reqs

--- converter.js
+++ converter.js
@@ -1,37 +1,56 @@
-var celsius = "";
-var fahrenheit = "";
+// Get a reference to the button element in the DOM
+var button = document.getElementById("converter");
 var clearBtn = document.getElementById("clear");
 var userinput = document.getElementById("temp-input");
 var output = document.getElementById("temp-output");
 
+button.addEventListener("checked", celsius);
+button.addEventListener("checked", fahren);
+// Assign a function to be executed when the button is clicked
+button.addEventListener("click", determineConverter);
+
 function toCelsius () {
-  celsius = (userinput - 32) / 1.8 
+  var userInputC = userinput.value;
+  var convertedC = ((userInputC - 32) / 1.8);
+  output.value = convertedC + " degrees Celsius"
+  if (convertedC > 32) {
+    output.style.color = "red";
+  } else if (convertedC <= 0) {
+    output.style.color = "blue";
+  } else {
+    output.style.color = "green";
+  }
 }
+
 
 function toFahrenheit () {
-  fahrenheit = userinput * 1.8 + 32
+  var userInputF = userinput.value;
+  var convertedF = ((userInputF * 1.8) + 32);
+  output.value = convertedF + " degrees Fahrenheit"
+  if (convertedF > 90) {
+    output.style.color= "red";
+  } else if (convertedF <= 32) {
+    output.style.color = "blue";
+  } else {
+    output.style.color = "green";
+  }
 }
 
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
+
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
-  if {
-    document.getElementById("celsius").checked
+  if (document.getElementById("celsius").checked) {
     toCelsius();
     console.log("celsius checked");
   }
-  else if {
-    document.getElementById("fahren").checked
+  else if (document.getElementById("fahren").checked) {
     toFahrenheit();
     console.log("fahrenheit checked");
   }
 }
 
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
 
 function enterKeyPressed(keypress){
   if (keypress.which === 13) {
@@ -41,9 +60,9 @@ function enterKeyPressed(keypress){
 
 function clear(){
   document.getElementById("temp-input").value = "";
-  document.getElementById(temp-output).innerHTML = "";
+  document.getElementById("temp-output").innerHTML = "";
 }
 
-clearBtn.addEventListener(click, clear);
+clearBtn.addEventListener("click", clear);
 
 document.addEventListener("keydown", enterKeyPressed);
